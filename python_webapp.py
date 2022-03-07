4e2820c7449e129c8cc5435dbdaaed23902ebb02
Guirassy <Fode.Guirassy@sicpa.com>
2022-03-07 11:06:59 -0500

    first commit

---
+++ python_webapp.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  7 09:19:52 2022
+
+@author: FGuirassy
+"""
+
+
+import pandas as pd  # pip install pandas openpyxl
+import plotly.express as px  # pip install plotly-express
+import streamlit as st  # pip install streamlit
+
+import pandas as pd
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+    
+st.write("""
+          
+This is web deployment testing 
+
+""")
