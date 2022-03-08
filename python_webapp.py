c1ee6c6c58a3ac38e75eb15f2292537763ee52ac
Fode Mahamadou Lamine GUIRASSY [SNT DSI/SMC/SUP] <GUIRASSY028358@orange-sonatel.com>
2022-03-08 11:57:32 -0500

    second commit

--- python_webapp.py
+++ python_webapp.py
@@ -1,34 +1,5 @@
-
-
-   
 import pandas as pd
 import streamlit as st
 import plotly.express as px
 from PIL import Image
-
-st.set_page_config(page_title='Survey Results')
-st.header('Survey Results 2021')
-st.subheader('Was the tutorial helpful?')
-
-### --- LOAD DATAFRAME
-excel_file = 'C:/Users/guirassy028358/Documents/Python Scripts/fode-reporting-web-tutorial-main/Survey_Results.xlsx'
-sheet_name = 'DATA'
-
-df = pd.read_excel(excel_file,
-                   sheet_name=sheet_name,
-                   usecols='B:D',
-                   header=3)
-
-df_participants = pd.read_excel(excel_file,
-                                sheet_name= sheet_name,
-                                usecols='F:G',
-                                header=3)
-df_participants.dropna(inplace=True)
-st.dataframe(df)
-# --- PLOT PIE CHART
-pie_chart = px.pie(df_participants,
-                title='Total No. of Participants',
-                values='Participants',
-                names='Departments')
-
-st.plotly_chart(pie_chart)
+st.markdown('Python is **_really_ cool**.')
