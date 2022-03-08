fb23e8dd6ac9c4f9e379fcfb5e8797d4ec7e072d
Fode Mahamadou Lamine GUIRASSY [SNT DSI/SMC/SUP] <GUIRASSY028358@orange-sonatel.com>
2022-03-08 00:24:46 -0500

    initial4 commit

--- python_webapp.py
+++ python_webapp.py
@@ -24,48 +24,7 @@ df_participants = pd.read_excel(excel_file,
                                 usecols='F:G',
                                 header=3)
 df_participants.dropna(inplace=True)
-
-# --- STREAMLIT SELECTION
-department = df['Department'].unique().tolist()
-ages = df['Age'].unique().tolist()
-
-age_selection = st.slider('Age:',
-                        min_value= min(ages),
-                        max_value= max(ages),
-                        value=(min(ages),max(ages)))
-
-department_selection = st.multiselect('Department:',
-                                    department,
-                                    default=department)
-
-# --- FILTER DATAFRAME BASED ON SELECTION
-mask = (df['Age'].between(*age_selection)) & (df['Department'].isin(department_selection))
-number_of_result = df[mask].shape[0]
-st.markdown(f'*Available Results: {number_of_result}*')
-
-# --- GROUP DATAFRAME AFTER SELECTION
-df_grouped = df[mask].groupby(by=['Rating']).count()[['Age']]
-df_grouped = df_grouped.rename(columns={'Age': 'Votes'})
-df_grouped = df_grouped.reset_index()
-
-# --- PLOT BAR CHART
-bar_chart = px.bar(df_grouped,
-                   x='Rating',
-                   y='Votes',
-                   text='Votes',
-                   color_discrete_sequence = ['#F63366']*len(df_grouped),
-                   template= 'plotly_white')
-st.plotly_chart(bar_chart)
-
-# --- DISPLAY IMAGE & DATAFRAME
-col1, col2 = st.beta_columns(2)
-image = Image.open('images/survey.jpg')
-print(image)
-col1.image(image,
-        caption='Designed by slidesgo / Freepik',
-        use_column_width=True)
-col2.dataframe(df[mask])
-
+st.dataframe(df)
 # --- PLOT PIE CHART
 pie_chart = px.pie(df_participants,
                 title='Total No. of Participants',
