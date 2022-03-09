23838ad7b27d4b9ecdefd0c420c0293f6727cf12
Fode Mahamadou Lamine GUIRASSY [SNT DSI/SMC/SUP] <GUIRASSY028358@orange-sonatel.com>
2022-03-08 21:28:46 -0500

    initial commit

--- python_webapp.py
+++ python_webapp.py
@@ -6,9 +6,13 @@ from PIL import Image
 st.set_page_config(page_title='Survey Results')
 st.header('Survey Results 2021')
 st.subheader('Was the tutorial helpful?')
-
+import os
 ### --- LOAD DATAFRAME
-excel_file = 'C:/report/fode-reporting-web-tutorial-main/Results.xlsx'
+mydir = 'C:/report/fode-reporting-web-tutorial-main/'
+excel_file = 'Results.xlsx'
+training_images_labels_path = os.path.join(mydir, excel_file)
+
+#excel_file = 'C:/report/fode-reporting-web-tutorial-main/Results.xlsx'
 sheet_name = 'DATA'
 
 df = pd.read_excel(excel_file,
