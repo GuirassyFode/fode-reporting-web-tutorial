be15bc502fd32059aa506ed80c03d4987d7e54f1
Fode Mahamadou Lamine GUIRASSY [SNT DSI/SMC/SUP] <GUIRASSY028358@orange-sonatel.com>
2022-03-08 22:01:52 -0500

    initial commit

--- python_webapp.py
+++ python_webapp.py
@@ -2,17 +2,18 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 from PIL import Image
+from openpyxl import Workbook
 
 st.set_page_config(page_title='Survey Results')
 st.header('Survey Results 2021')
 st.subheader('Was the tutorial helpful?')
 import os
 ### --- LOAD DATAFRAME
-mydir = 'C:/report/fode-reporting-web-tutorial-main/'
-excel_file = 'Results.xlsx'
-training_images_labels_path = os.path.join(mydir, excel_file)
+# mydir = 'C:/report/fode-reporting-web-tutorial-main/'
+# excel_file = 'Results.xlsx'
+# training_images_labels_path = os.path.join(mydir, excel_file)
 
-#excel_file = 'C:/report/fode-reporting-web-tutorial-main/Results.xlsx'
+excel_file = 'C:/report/fode-reporting-web-tutorial-main/Results.xlsx'
 sheet_name = 'DATA'
 
 df = pd.read_excel(excel_file,
